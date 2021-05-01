aefc0d8b8394cab14d78a74e817f17b38e59fb83
Adam Reszka <adam.reszka85@gmail.com>
2021-05-01 19:35:29 +0200

    changed rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import babel from 'rollup-plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
+import resolve, { nodeResolve } from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
 import commonjs from 'rollup-plugin-commonjs';
@@ -8,6 +8,10 @@ import commonjs from 'rollup-plugin-commonjs';
 export default [
     {
         input: './src/index.js',
+        globals: {
+            'react': 'React',
+            'react-dom': 'ReactDOM'
+        },
         output: [
             {
                 file: 'dist/index.js',
@@ -26,6 +30,11 @@ export default [
             }),
             external(),
             resolve(),
+            nodeResolve({
+                ignoreGlobal: false,
+                include: ['node_modules/**'],
+                skip: ['react', 'react-dom'],
+              }),
             terser(),
             commonjs({
                 namedExports: {
